7b5a7473ccb373ded290c80f8fe0f949897ce8dc
sahiruLakshan <sahiru906@gmail.com>
2023-12-03 12:45:17 +0530

    new changes

--- Backend/Routes/product.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const producController = require('../Controllers/product');
-
-router.post('/add', producController.addProduct);
-router.get('/product/:id', producController.getProduct)
-router.get('/product', producController.getAllProduct)
-module.exports = router
